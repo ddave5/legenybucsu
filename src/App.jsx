ac748e8dbf59ba86cb4bfe0058f3bfaf1f3d40cf
kanyasijozsef <h860179@stud.u-szeged.hu>
2022-07-21 22:19:43 +0200

    first update

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import TaskList from './feladatok.json';
+
+function App() {
+
+  const [feladat, setFeladat] = useState();
+  let lastfeladat = TaskList.at(0);
+
+  const sorsolas = () => {
+    let tmp = undefined
+    while (1){
+      tmp = TaskList.at(Math.floor(Math.random() * TaskList.length));
+      if( tmp.feladat !== lastfeladat.feladat) break;
+    } 
+    lastfeladat = feladat
+    setFeladat(tmp)
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Legénybúcsú</h1>
+      <div className='gombDiv'>
+        <button onClick={sorsolas} > {feladat === undefined ? 'Pörgetés' : 'Újrapörgetés' }</button>
+        </div>
+      
+      { feladat !== undefined && (
+        <>
+          <div className='titleDiv'>
+            <h2>{feladat.title}</h2>
+          </div> 
+          <div className='feladatDiv'>
+            <p>{feladat.feladat}</p>
+          </div>
+        </>
+      )}
+    </React.Fragment>
+  );
+}
+
+export default App;
